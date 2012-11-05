04482e337dda884d130701e59ab523ee6647f34d
SCRAPPYDOO <scrapek69@gmail.com>
2012-11-05 22:28:44 +0100

    rev 5 : Setting up Main Menu activity

---
+++ src/com/example/spiderapi/CMenu.java
@@ -0,0 +1,29 @@
+package com.example.spiderapi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CMenu extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.menu_backg_sound);
+		
+		Thread timer = new Thread();
+		{
+			
+	
+		}
+		
+	}
+
+	
+	
+	
+	
+	
+}
