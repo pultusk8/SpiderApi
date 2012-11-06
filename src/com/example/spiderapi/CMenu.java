61610619aef9727fd101c87f3b3ae25fee164f7d
SCRAPPYDOO <scrapek69@gmail.com>
2012-11-06 14:24:36 +0100

    rev 6 : Added Loading Screen
    class CMenu // our loading screen activity

--- src/com/example/spiderapi/CMenu.java
+++ src/com/example/spiderapi/CMenu.java
@@ -1,6 +1,7 @@
 package com.example.spiderapi;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class CMenu extends Activity
@@ -13,17 +14,36 @@ public class CMenu extends Activity
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.menu_backg_sound);
 		
-		Thread timer = new Thread();
+		Thread tTimer = new Thread()
 		{
-			
-	
-		}
+			public void run()
+			{
+				try
+				{
+					sleep(5000);
+				}
+				catch (InterruptedException e)
+				{
+					e.printStackTrace();
+				}
+				finally
+				{
+					Intent MainMenu = new Intent("com.example.spiderapi.ACTIVITYCORE");
+					startActivity(MainMenu);
+				}
+			}
+		};
 		
+		tTimer.start();
 	}
 
-	
-	
-	
+	@Override
+	protected void onPause() 
+	{
+		// TODO Auto-generated method stub
+		super.onPause();
+		finish();
+	}
 	
 	
 }
