e9cf6f6299998309aac0a5754d2eba50ec54feb7
SCRAPPYDOO <scrapek69@gmail.com>
2013-02-23 05:34:05 +0100

    rev 54 : MainMenu

---
+++ src/com/example/spiderapi/MainMenu.java
@@ -0,0 +1,20 @@
+package com.example.spiderapi;
+
+import android.graphics.Bitmap;
+
+enum MenuState
+{
+	MainMenu,
+	Options,
+	Developers,
+}
+
+public class MainMenu 
+{
+	private Bitmap bmpBitmap = null;
+	/*
+	 * caly szkielet Main menu z wejsciem do wszytkich opcji
+	 * 
+	 * 
+	 */
+}
